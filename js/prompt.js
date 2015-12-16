e067639adf6a61e59833a9feba267192e97f5d64
ctinaa <christina.fcook@yahoo.com>
2015-12-16 01:28:10 -0800

    Needs to add projects

--- js/prompt.js
+++ js/prompt.js
@@ -2,26 +2,23 @@
         $("#name").keypress(
             function (event) {
                 if (event.which == 13) {
-                        var usersName = document.getElementById("name");
-        				var displayBox = document.getElementById("displayName");
-     				    usersName.style.display = "none";
-         			  displayBox.innerHTML = usersName.value + ".";
-        			  displayBox.style.display = "inline";
+                      var usersName = document.getElementById("name");
+                			var displayBox = document.getElementById("displayName");
+             				  usersName.style.display = "none";
+                 			displayBox.innerHTML = usersName.value + ".";
+                			displayBox.style.display = "inline";
+                                
+                      // The response given when pressing Enter
+                      var response = document.getElementById("respond");
+                      $("#respond").hide().fadeIn(3000);
+                      response.innerHTML = "I'm Christina.";
 
-                        
-
-                        var response = document.getElementById("respond");
-                        $("#respond").hide().fadeIn(3000);
-                        response.innerHTML = "I'm Christina.";
-                        String.fromCharCode(<8>);
 
                         // $("span").hide().fadeIn(3000);
                         // response.innerHTML = "Hi, I'm Christina.";
-                          $("element").hide();
+                        
                     }
                }
             );
-        /* Type.js*/
-
-      });
+});
 
